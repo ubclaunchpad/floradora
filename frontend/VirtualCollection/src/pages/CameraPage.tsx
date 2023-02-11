90c6964ab020a50378f65aa6135848e6225ae77b
marvelhariadi <marvelhariadi@gmail.com>
2023-02-11 11:32:45 -0800

    ahh

--- frontend/VirtualCollection/src/pages/CameraPage.tsx
+++ frontend/VirtualCollection/src/pages/CameraPage.tsx
@@ -58,7 +58,7 @@ export default class CameraPage {
                 type = {this.state.type}
                 ref = {ref => {
                     this.camera = ref;
-                }}>
+                }}> 
 
                 </Camera>
 
