8e7ae8138778a50cbc193dc2e030132a3e12e3e2
Louie Weng <louieweng@outlook.com>
2023-02-11 16:56:46 -0800

    added linting and tests for backend

---
+++ backend/tests/Backend.test.tsx
@@ -0,0 +1,18 @@
+import request from 'supertest';
+
+import server from '../Backend';
+
+afterAll(async () => {
+    server.close();
+});
+
+describe('Test the isalive path', () => {
+    test('It should response the GET method', (done) => {
+        request(server)
+            .get('/isalive')
+            .then((response) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    });
+});
