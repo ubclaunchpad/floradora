d85e739f039da35aaf099be448aeab55c673265f
Louie Weng <louieweng@outlook.com>
2023-03-11 00:06:34 -0800

    added design styles for home page

---
+++ frontend/VirtualCollection/src/styles/buttons.tsx
@@ -0,0 +1,15 @@
+export const roundedCorners = {
+    borderRadius: 50,
+};
+
+export const longLength = {
+    width: 250,
+};
+
+export const verticalPadding = {
+    paddingVertical: 12,
+};
+
+export const buttonText = {
+    textAlign: 'center' as const,
+};
