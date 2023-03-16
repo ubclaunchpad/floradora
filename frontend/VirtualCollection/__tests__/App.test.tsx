dcfbe11a279d23da9d78343ccbe36d709f4d2fe9
Louie Weng <louieweng@outlook.com>
2023-03-16 01:23:41 -0700

    added initial google auth connection to grab user information

--- frontend/VirtualCollection/__tests__/App.test.tsx
+++ frontend/VirtualCollection/__tests__/App.test.tsx
@@ -3,6 +3,8 @@ import renderer from 'react-test-renderer';
 
 import App from '../App';
 
+jest.mock('expo-linking');
+
 describe('<App />', () => {
     it('matches snapshot', () => {
         const tree = renderer.create(<App />).toJSON();
--- frontend/VirtualCollection/src/pages/LoginPage.tsx
+++ frontend/VirtualCollection/src/pages/LoginPage.tsx
@@ -1,8 +1,48 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { ButtonStyle, Colors, Spacings, Texts } from '../styles';
-import { AppStackProps } from './types';
 
-export default function LoginPage({ navigation }: AppStackProps) {
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect, useState } from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginPage() {
+    const [token, setToken] = useState('');
+    const [userInfo, setUserInfo] = useState(null);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        expoClientId: '485163119792-dbqlktvmh8046q5jaef2ton1riuvq9hs',
+        iosClientId: '485163119792-dbqlktvmh8046q5jaef2ton1riuvq9hs.apps.googleusercontent.com',
+        androidClientId: '485163119792-odmfe32gi9mgrib6qc953ctvgoq6p0kd.apps.googleusercontent.com',
+        redirectUri: 'com.app.floradora:/oauthredirect',
+    });
+
+    useEffect(() => {
+        if (response?.type === 'success') {
+            const token = response.authentication?.accessToken;
+            if (token) {
+                setToken(token);
+                getUserInfo();
+            }
+        }
+    }, [response, token]);
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+
+            const user = await response.json();
+            setUserInfo(user);
+        } catch (error) {
+            console.log(`Error on sign-in: ${error}`);
+        }
+        console.log(userInfo);
+    };
+
     return (
         <View style={styles.pageContainer}>
             <View style={styles.circle}></View>
@@ -10,7 +50,7 @@ export default function LoginPage({ navigation }: AppStackProps) {
                 <Text style={styles.titleText}>Virtual Collection</Text>
                 <Text style={styles.subheadingText}>Explore with us</Text>
             </View>
-            <Pressable style={styles.loginButton} onPress={() => navigation.navigate('Home')}>
+            <Pressable style={styles.loginButton} onPress={() => promptAsync()}>
                 <Text style={styles.loginButtonText}>Login with Google</Text>
             </Pressable>
         </View>
