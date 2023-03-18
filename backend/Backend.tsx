e4910c5ae39c9731b299e773f03a2f489484344c
Jacob Wu <99063771+wu-jacob@users.noreply.github.com>
2023-03-18 11:33:53 -0700

    ran prettier

--- backend/Backend.tsx
+++ backend/Backend.tsx
@@ -1,29 +1,30 @@
-import express from "express";
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import { Server } from 'http';
+import pool from './db';
 const app = express();
-import cors from "cors";
-import pool from "./db";
-import {Request, Response} from 'express'
-import { Server } from "http";
 
 // middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json()); //req body
 
 // local listener
 const server: Server = app.listen(8080, () => {
-    console.log("Server is listning on port 8080")
+    console.log('Server is listning on port 8080');
 });
 export default server;
-
 
 //ROUTES//
 
 //create a user
 
-app.post("/users", async(req: Request, res: Response) => {
+app.post('/users', async (req: Request, res: Response) => {
     try {
-        const {email, display_name} = req.body;
-        const newUser = await pool.query("INSERT INTO users (email, display_name) VALUES ($1, $2);", [email, display_name]);
+        const { email, display_name } = req.body;
+        const newUser = await pool.query(
+            'INSERT INTO users (email, display_name) VALUES ($1, $2);',
+            [email, display_name]
+        );
 
         res.json(newUser);
     } catch (error) {
@@ -31,63 +32,85 @@ app.post("/users", async(req: Request, res: Response) => {
     }
 });
 
-
 //get all users
 
-app.get("/users", async(req: Request, res: Response) => {
+app.get('/users', async (req: Request, res: Response) => {
     try {
-        const allUsers = await pool.query("SELECT * FROM users");
+        const allUsers = await pool.query('SELECT * FROM users');
         res.json(allUsers.rows);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //get a user
 
-app.get("/users/id", async(req: Request, res: Response) => {
+app.get('/users/id', async (req: Request, res: Response) => {
     try {
-        const {user_id} = req.body;
-        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [user_id]);
+        const { user_id } = req.body;
+        const user = await pool.query('SELECT * FROM users WHERE user_id = $1', [user_id]);
         res.json(user.rows[0]);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 // update a user
 
-app.put("/users/id", async(req: Request, res: Response) => {
+app.put('/users/id', async (req: Request, res: Response) => {
     try {
-        const {user_id, email, display_name} = req.body;
-        await pool.query("UPDATE users SET display_name = $1, email = $2 WHERE user_id = $3", [display_name, email, user_id]);
-        res.json("User " + user_id + " was updated.");
+        const { user_id, email, display_name } = req.body;
+        await pool.query('UPDATE users SET display_name = $1, email = $2 WHERE user_id = $3', [
+            display_name,
+            email,
+            user_id,
+        ]);
+        res.json('User ' + user_id + ' was updated.');
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //delete a user
 
-app.delete("/users", async(req: Request, res: Response) => {
+app.delete('/users', async (req: Request, res: Response) => {
     try {
-        const {user_id} = req.body;
-        await pool.query("DELETE FROM users WHERE user_id = $1", [user_id]);
-        res.json("User" + user_id + "was deleted.");
+        const { user_id } = req.body;
+        await pool.query('DELETE FROM users WHERE user_id = $1', [user_id]);
+        res.json('User' + user_id + 'was deleted.');
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //create an observation
 
-app.post("/observations", async(req: Request, res: Response) => {
+app.post('/observations', async (req: Request, res: Response) => {
     try {
-        const {user_id, plant_type_id, date_created, 
-            image_url, confidence_score, longitude, latitude, description} = req.body;
-        const newObservation = await pool.query("INSERT INTO observations (user_id, plant_type_id, date_created, image_url, "
-            + "confidence_score, longitude, latitude, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", 
-            [user_id, plant_type_id, date_created, image_url, confidence_score, longitude, latitude, description]);
+        const {
+            user_id,
+            plant_type_id,
+            date_created,
+            image_url,
+            confidence_score,
+            longitude,
+            latitude,
+            description,
+        } = req.body;
+        const newObservation = await pool.query(
+            'INSERT INTO observations (user_id, plant_type_id, date_created, image_url, ' +
+                'confidence_score, longitude, latitude, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);',
+            [
+                user_id,
+                plant_type_id,
+                date_created,
+                image_url,
+                confidence_score,
+                longitude,
+                latitude,
+                description,
+            ]
+        );
 
         res.json(newObservation);
     } catch (error) {
@@ -97,44 +120,49 @@ app.post("/observations", async(req: Request, res: Response) => {
 
 //get all observations
 
-app.get("/observations", async(req: Request, res: Response) => {
+app.get('/observations', async (req: Request, res: Response) => {
     try {
-        const allObservations = await pool.query("SELECT * FROM observations");
+        const allObservations = await pool.query('SELECT * FROM observations');
         res.json(allObservations.rows);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //get an observation
 
-app.get("/observations/id", async(req: Request, res: Response) => {
+app.get('/observations/id', async (req: Request, res: Response) => {
     try {
-        const {observation_id} = req.body;
-        const observation = await pool.query("SELECT * FROM observations WHERE observation_id = $1", [observation_id]);
+        const { observation_id } = req.body;
+        const observation = await pool.query(
+            'SELECT * FROM observations WHERE observation_id = $1',
+            [observation_id]
+        );
         res.json(observation.rows[0]);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 // delete an observation
 
-app.delete("/observations", async(req: Request, res: Response) => {
+app.delete('/observations', async (req: Request, res: Response) => {
     try {
-        const {observation_id} = req.body;
-        await pool.query("DELETE FROM observations WHERE observation_id = $1", [observation_id]);
-        res.json("Observation" + observation_id + "was deleted.");
+        const { observation_id } = req.body;
+        await pool.query('DELETE FROM observations WHERE observation_id = $1', [observation_id]);
+        res.json('Observation' + observation_id + 'was deleted.');
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 // perform plant_types operations
-app.post("/plant_types", async(req: Request, res: Response) => {
+app.post('/plant_types', async (req: Request, res: Response) => {
     try {
-        const {plant_name} = req.body;
-        const newPlantType = await pool.query("INSERT INTO plant_types (plant_name) VALUES ($1);", [plant_name]);
+        const { plant_name } = req.body;
+        const newPlantType = await pool.query('INSERT INTO plant_types (plant_name) VALUES ($1);', [
+            plant_name,
+        ]);
 
         res.json(newPlantType);
     } catch (error) {
@@ -142,50 +170,54 @@ app.post("/plant_types", async(req: Request, res: Response) => {
     }
 });
 
-
 //get all plant_types
 
-app.get("/plant_types", async(req: Request, res: Response) => {
+app.get('/plant_types', async (req: Request, res: Response) => {
     try {
-        const allPlantTypes = await pool.query("SELECT * FROM plant_types");
+        const allPlantTypes = await pool.query('SELECT * FROM plant_types');
         res.json(allPlantTypes.rows);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //get a plant_type by id
 
-app.get("/plant_types/id", async(req: Request, res: Response) => {
+app.get('/plant_types/id', async (req: Request, res: Response) => {
     try {
-        const {plant_type_id} = req.body;
-        const plantType = await pool.query("SELECT * FROM plant_types WHERE plant_type_id = $1", [plant_type_id]);
+        const { plant_type_id } = req.body;
+        const plantType = await pool.query('SELECT * FROM plant_types WHERE plant_type_id = $1', [
+            plant_type_id,
+        ]);
         res.json(plantType.rows[0]);
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 //update a plant_name by its id
 
-app.put("/plant_types/id", async(req: Request, res: Response) => {
+app.put('/plant_types/id', async (req: Request, res: Response) => {
     try {
-        const {plant_type_id, plant_name} = req.body;
-        await pool.query("UPDATE plant_types SET plants_name = $1 WHERE plant_type_id = $2", [plant_name, plant_type_id]);
-        res.json("Plant " + plant_type_id + " was updated.");
+        const { plant_type_id, plant_name } = req.body;
+        await pool.query('UPDATE plant_types SET plants_name = $1 WHERE plant_type_id = $2', [
+            plant_name,
+            plant_type_id,
+        ]);
+        res.json('Plant ' + plant_type_id + ' was updated.');
     } catch (error) {
         console.error(error);
     }
-})
+});
 
 // // delete a plant_type
 
-app.delete("/plant_types", async(req: Request, res: Response) => {
+app.delete('/plant_types', async (req: Request, res: Response) => {
     try {
-        const {plant_type_id} = req.body;
-        await pool.query("DELETE FROM plant_types WHERE plant_type_id = $1", [plant_type_id]);
-        res.json("Plant Type" + plant_type_id + "was deleted.");
+        const { plant_type_id } = req.body;
+        await pool.query('DELETE FROM plant_types WHERE plant_type_id = $1', [plant_type_id]);
+        res.json('Plant Type' + plant_type_id + 'was deleted.');
     } catch (error) {
         console.error(error);
     }
-})
+});
--- backend/dist/Backend.js
+++ backend/dist/Backend.js
@@ -12,24 +12,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("./db"));
+const app = (0, express_1.default)();
 // middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json()); //req body
 // local listener
 const server = app.listen(8080, () => {
-    console.log("Server is listning on port 8080");
+    console.log('Server is listning on port 8080');
 });
 exports.default = server;
 //ROUTES//
 //create a user
-app.post("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { email, display_name } = req.body;
-        const newUser = yield db_1.default.query("INSERT INTO users (email, display_name) VALUES ($1, $2);", [email, display_name]);
+        const newUser = yield db_1.default.query('INSERT INTO users (email, display_name) VALUES ($1, $2);', [email, display_name]);
         res.json(newUser);
     }
     catch (error) {
@@ -37,9 +37,9 @@ app.post("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     }
 }));
 //get all users
-app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const allUsers = yield db_1.default.query("SELECT * FROM users");
+        const allUsers = yield db_1.default.query('SELECT * FROM users');
         res.json(allUsers.rows);
     }
     catch (error) {
@@ -47,10 +47,10 @@ app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     }
 }));
 //get a user
-app.get("/users/id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/users/id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { user_id } = req.body;
-        const user = yield db_1.default.query("SELECT * FROM users WHERE user_id = $1", [user_id]);
+        const user = yield db_1.default.query('SELECT * FROM users WHERE user_id = $1', [user_id]);
         res.json(user.rows[0]);
     }
     catch (error) {
@@ -58,33 +58,46 @@ app.get("/users/id", (req, res) => __awaiter(void 0, void 0, void 0, function* (
     }
 }));
 // update a user
-app.put("/users/id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.put('/users/id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { user_id, email, display_name } = req.body;
-        yield db_1.default.query("UPDATE users SET display_name = $1, email = $2 WHERE user_id = $3", [display_name, email, user_id]);
-        res.json("User " + user_id + " was updated.");
+        yield db_1.default.query('UPDATE users SET display_name = $1, email = $2 WHERE user_id = $3', [
+            display_name,
+            email,
+            user_id,
+        ]);
+        res.json('User ' + user_id + ' was updated.');
     }
     catch (error) {
         console.error(error);
     }
 }));
 //delete a user
-app.delete("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.delete('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { user_id } = req.body;
-        yield db_1.default.query("DELETE FROM users WHERE user_id = $1", [user_id]);
-        res.json("User" + user_id + "was deleted.");
+        yield db_1.default.query('DELETE FROM users WHERE user_id = $1', [user_id]);
+        res.json('User' + user_id + 'was deleted.');
     }
     catch (error) {
         console.error(error);
     }
 }));
 //create an observation
-app.post("/observations", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/observations', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { user_id, plant_type_id, date_created, image_url, confidence_score, longitude, latitude, description } = req.body;
-        const newObservation = yield db_1.default.query("INSERT INTO observations (user_id, plant_type_id, date_created, image_url, "
-            + "confidence_score, longitude, latitude, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", [user_id, plant_type_id, date_created, image_url, confidence_score, longitude, latitude, description]);
+        const { user_id, plant_type_id, date_created, image_url, confidence_score, longitude, latitude, description, } = req.body;
+        const newObservation = yield db_1.default.query('INSERT INTO observations (user_id, plant_type_id, date_created, image_url, ' +
+            'confidence_score, longitude, latitude, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);', [
+            user_id,
+            plant_type_id,
+            date_created,
+            image_url,
+            confidence_score,
+            longitude,
+            latitude,
+            description,
+        ]);
         res.json(newObservation);
     }
     catch (error) {
@@ -92,9 +105,9 @@ app.post("/observations", (req, res) => __awaiter(void 0, void 0, void 0, functi
     }
 }));
 //get all observations
-app.get("/observations", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/observations', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const allObservations = yield db_1.default.query("SELECT * FROM observations");
+        const allObservations = yield db_1.default.query('SELECT * FROM observations');
         res.json(allObservations.rows);
     }
     catch (error) {
@@ -102,10 +115,10 @@ app.get("/observations", (req, res) => __awaiter(void 0, void 0, void 0, functio
     }
 }));
 //get an observation
-app.get("/observations/id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/observations/id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { observation_id } = req.body;
-        const observation = yield db_1.default.query("SELECT * FROM observations WHERE observation_id = $1", [observation_id]);
+        const observation = yield db_1.default.query('SELECT * FROM observations WHERE observation_id = $1', [observation_id]);
         res.json(observation.rows[0]);
     }
     catch (error) {
@@ -113,21 +126,23 @@ app.get("/observations/id", (req, res) => __awaiter(void 0, void 0, void 0, func
     }
 }));
 // delete an observation
-app.delete("/observations", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.delete('/observations', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { observation_id } = req.body;
-        yield db_1.default.query("DELETE FROM observations WHERE observation_id = $1", [observation_id]);
-        res.json("Observation" + observation_id + "was deleted.");
+        yield db_1.default.query('DELETE FROM observations WHERE observation_id = $1', [observation_id]);
+        res.json('Observation' + observation_id + 'was deleted.');
     }
     catch (error) {
         console.error(error);
     }
 }));
 // perform plant_types operations
-app.post("/plant_types", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/plant_types', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { plant_name } = req.body;
-        const newPlantType = yield db_1.default.query("INSERT INTO plant_types (plant_name) VALUES ($1);", [plant_name]);
+        const newPlantType = yield db_1.default.query('INSERT INTO plant_types (plant_name) VALUES ($1);', [
+            plant_name,
+        ]);
         res.json(newPlantType);
     }
     catch (error) {
@@ -135,9 +150,9 @@ app.post("/plant_types", (req, res) => __awaiter(void 0, void 0, void 0, functio
     }
 }));
 //get all plant_types
-app.get("/plant_types", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/plant_types', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const allPlantTypes = yield db_1.default.query("SELECT * FROM plant_types");
+        const allPlantTypes = yield db_1.default.query('SELECT * FROM plant_types');
         res.json(allPlantTypes.rows);
     }
     catch (error) {
@@ -145,10 +160,12 @@ app.get("/plant_types", (req, res) => __awaiter(void 0, void 0, void 0, function
     }
 }));
 //get a plant_type by id
-app.get("/plant_types/id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/plant_types/id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { plant_type_id } = req.body;
-        const plantType = yield db_1.default.query("SELECT * FROM plant_types WHERE plant_type_id = $1", [plant_type_id]);
+        const plantType = yield db_1.default.query('SELECT * FROM plant_types WHERE plant_type_id = $1', [
+            plant_type_id,
+        ]);
         res.json(plantType.rows[0]);
     }
     catch (error) {
@@ -156,22 +173,25 @@ app.get("/plant_types/id", (req, res) => __awaiter(void 0, void 0, void 0, funct
     }
 }));
 //update a plant_name by its id
-app.put("/plant_types/id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.put('/plant_types/id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { plant_type_id, plant_name } = req.body;
-        yield db_1.default.query("UPDATE plant_types SET plants_name = $1 WHERE plant_type_id = $2", [plant_name, plant_type_id]);
-        res.json("Plant " + plant_type_id + " was updated.");
+        yield db_1.default.query('UPDATE plant_types SET plants_name = $1 WHERE plant_type_id = $2', [
+            plant_name,
+            plant_type_id,
+        ]);
+        res.json('Plant ' + plant_type_id + ' was updated.');
     }
     catch (error) {
         console.error(error);
     }
 }));
 // // delete a plant_type
-app.delete("/plant_types", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.delete('/plant_types', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { plant_type_id } = req.body;
-        yield db_1.default.query("DELETE FROM plant_types WHERE plant_type_id = $1", [plant_type_id]);
-        res.json("Plant Type" + plant_type_id + "was deleted.");
+        yield db_1.default.query('DELETE FROM plant_types WHERE plant_type_id = $1', [plant_type_id]);
+        res.json('Plant Type' + plant_type_id + 'was deleted.');
     }
     catch (error) {
         console.error(error);
