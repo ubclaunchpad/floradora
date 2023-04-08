8aa01a2253750f858da13a931c24a7c4dd113562
Sarah Bornais <sarah.bornais@gmail.com>
2023-04-08 11:42:55 -0700

    Clean up camera page

--- frontend/VirtualCollection/src/pages/CameraPage.tsx
+++ frontend/VirtualCollection/src/pages/CameraPage.tsx
@@ -1,90 +1,79 @@
-import { Camera, CameraType } from 'expo-camera';
-import { StatusBar } from 'expo-status-bar';
-import React, { useRef, useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
-import { Button, Image, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    //init and set permission variables
-    let cameraRef = useRef<Camera>(null);
-    const [hasCameraPermission, setHasCameraPermission] = useState(false);              // inits as false to be reset later on as true
-    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);  // won't work without being set w false first
-    const [permission, requestPermission] = Camera.useCameraPermissions();              // from the beforetimes
-    type Photo = { uri: string };  // exists to stop photo from complaining
-    const [photo, setPhoto] = useState<Photo | undefined>(); // tried to do useState() but would not work???
-    const [type, setType] = useState(CameraType.back); // to change to back cam
-    
-    //ask permission before starting
+export default function CameraPage() {
+    // Init and set permission variables
+    const cameraRef = useRef<Camera>(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(false);
+    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);
+
+    type Photo = { uri: string }; // exists to stop photo from complaining
+    const [photo, setPhoto] = useState<Photo | undefined>();
+
+    // Ask permission before starting
     useEffect(() => {
         (async () => {
             const cameraPermission = await Camera.requestCameraPermissionsAsync();
             const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
-            setHasCameraPermission(cameraPermission.status == "granted");
-            setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+            setHasCameraPermission(cameraPermission.status == 'granted');
+            setHasMediaLibraryPermission(mediaLibraryPermission.status === 'granted');
         })();
     }, []);
-    
-    // prompt enabling permission
-    if (hasCameraPermission === false || hasCameraPermission === undefined){
-        return <Text>Requesting permissions...</Text>
-    } else if (!hasCameraPermission){
-        //return <Text> Permission for camera not granted. Please change this in settings.</Text>
-        requestPermission(); // requests permissions
-    } 
-    
-    // back camera functionality (todo?)
-    // const [type, setType] = useState(CameraType.back);
-    // const [uri, setUri] = useState('');
-    // const camera = useRef<Camera>(null); ignore this jungle
 
-    function toggleCameraType() {
-        setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back));
+    // Prompt enabling permission
+    if (hasCameraPermission === false || hasCameraPermission === undefined) {
+        return <Text>Requesting permissions...</Text>;
     }
 
     // Camera UI
-    let takePic = async () => {
-        if (cameraRef.current){
-        let options = {
-            quality: 1,
-            base64: true,
-            exif: false
+    const takePic = async () => {
+        if (cameraRef.current) {
+            const options = {
+                quality: 1,
+                base64: true,
+                exif: false,
+            };
+            (cameraRef.current as Camera)
+                .takePictureAsync(options)
+                .then((photo) => setPhoto(photo));
+        }
     };
-                            //cameraRef no longer complains after typecasting
-        let newPhoto = await (cameraRef.current as Camera).takePictureAsync(options); 
-        setPhoto(newPhoto);
-    }
-};
 
-    // save and view photo 
-    if (photo){
-        let savePhoto = () => { //can save photo
+    // Save and view photo
+    if (photo) {
+        const savePhoto = () => {
+            // Can save photo
             MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-                setPhoto(undefined); //discards photo after saved
+                setPhoto(undefined); // Discards photo after saved
             });
         };
-     
+
         return (
             <SafeAreaView style={styles.container}>
-                <Image style={styles.preview} source={{ uri: "data:image/jpg" + photo }} />
-                {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined }
-                <Button title="Discard" onPress={() => setPhoto(undefined)} /> 
+                <Image style={styles.preview} source={{ uri: photo.uri }} />
+                {hasMediaLibraryPermission ? (
+                    <Button title="Save" onPress={savePhoto} />
+                ) : undefined}
+                <Button title="Discard" onPress={() => setPhoto(undefined)} />
             </SafeAreaView>
-        )
+        );
     }
 
     return (
-        <Camera style = {styles.container} ref={cameraRef}> 
+        <Camera style={styles.container} ref={cameraRef}>
             <View style={styles.buttonContainer}>
-                <Button title = "Snap a Picture" onPress={takePic} /> 
-                <Button title = "Flip" onPress={toggleCameraType} /> 
-            </View> 
+                <Button title="Snap a Picture" onPress={takePic} />
+            </View>
             <StatusBar style="auto" />
         </Camera>
     );
 }
 
-  // style guideline
-  const styles = StyleSheet.create({
+// Style guideline
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
@@ -93,11 +82,11 @@ export default function App() {
 
     buttonContainer: {
         backgroundColor: '#ffff',
-        alignSelf: 'flex-end'
+        alignSelf: 'flex-end',
     },
 
     preview: {
         alignSelf: 'stretch',
-        flex: 1
-    }
+        flex: 1,
+    },
 });
