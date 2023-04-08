42f9fadbc96517e7d36aa09ce6921b89bb1c8fe6
Marvel <77342694+marvelhariadi@users.noreply.github.com>
2023-04-08 11:54:44 -0700

    8 camera component (#34)
    * main code done. needs expo imports for setState
    
    * ahhhh
    
    * ahh
    
    * Fix camera functionality
    
    * the slay???
    
    * the slay
    
    * Added save picture to camera.
    
    * Ref now works
    
    * ref issue resolved.
    
    * setup backend
    
    * added empty line at the end of files
    
    * Add link to backend setup guide in main readme
    
    * added linting and tests for backend
    
    * Add backend directory structure
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * added design styles for home page
    
    * Add functionality to connet to database
    
    * Add table functionality use dbmulti
    
    * added database schema to /backend/db
    
    * Added post/get methods
    
    * added all CRUD operations for users table
    
    * added CRUD operations for all tables in database
    
    * fixed typos
    
    * migrated database info to .env file
    
    * ran prettier
    
    * added dist/ to .env backend
    
    * added dotenv dependency
    
    * deleted .env
    
    * added isalive test
    
    * added a newline somewhere
    
    * implement api endpoints
    
    * test api endpoints
    
    * ran prettier
    
    * Clean up camera page
    
    ---------
    
    Co-authored-by: Sarah Bornais <sarah.bornais@gmail.com>
    Co-authored-by: allwelly <78426047+allwelly@users.noreply.github.com>
    Co-authored-by: Louie Weng <louieweng@outlook.com>
    Co-authored-by: Zeo Wang <zeowang131@outlook.com>
    Co-authored-by: Jacob Wu <99063771+wu-jacob@users.noreply.github.com>

--- frontend/VirtualCollection/src/pages/CameraPage.tsx
+++ frontend/VirtualCollection/src/pages/CameraPage.tsx
@@ -1,11 +1,92 @@
-import { Button, Text, View } from 'react-native';
-import { AppStackProps } from './types';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function CameraPage({ navigation }: AppStackProps) {
+export default function CameraPage() {
+    // Init and set permission variables
+    const cameraRef = useRef<Camera>(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(false);
+    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);
+
+    type Photo = { uri: string }; // exists to stop photo from complaining
+    const [photo, setPhoto] = useState<Photo | undefined>();
+
+    // Ask permission before starting
+    useEffect(() => {
+        (async () => {
+            const cameraPermission = await Camera.requestCameraPermissionsAsync();
+            const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+            setHasCameraPermission(cameraPermission.status == 'granted');
+            setHasMediaLibraryPermission(mediaLibraryPermission.status === 'granted');
+        })();
+    }, []);
+
+    // Prompt enabling permission
+    if (hasCameraPermission === false || hasCameraPermission === undefined) {
+        return <Text>Requesting permissions...</Text>;
+    }
+
+    // Camera UI
+    const takePic = async () => {
+        if (cameraRef.current) {
+            const options = {
+                quality: 1,
+                base64: true,
+                exif: false,
+            };
+            (cameraRef.current as Camera)
+                .takePictureAsync(options)
+                .then((photo) => setPhoto(photo));
+        }
+    };
+
+    // Save and view photo
+    if (photo) {
+        const savePhoto = () => {
+            // Can save photo
+            MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+                setPhoto(undefined); // Discards photo after saved
+            });
+        };
+
+        return (
+            <SafeAreaView style={styles.container}>
+                <Image style={styles.preview} source={{ uri: photo.uri }} />
+                {hasMediaLibraryPermission ? (
+                    <Button title="Save" onPress={savePhoto} />
+                ) : undefined}
+                <Button title="Discard" onPress={() => setPhoto(undefined)} />
+            </SafeAreaView>
+        );
+    }
+
     return (
-        <View>
-            <Text>Camera</Text>
-            <Button title="Home" onPress={() => navigation.goBack()} />
-        </View>
+        <Camera style={styles.container} ref={cameraRef}>
+            <View style={styles.buttonContainer}>
+                <Button title="Snap a Picture" onPress={takePic} />
+            </View>
+            <StatusBar style="auto" />
+        </Camera>
     );
 }
+
+// Style guideline
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    buttonContainer: {
+        backgroundColor: '#ffff',
+        alignSelf: 'flex-end',
+    },
+
+    preview: {
+        alignSelf: 'stretch',
+        flex: 1,
+    },
+});
