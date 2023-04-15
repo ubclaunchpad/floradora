319acb2189d3139f955e423a9e49b4a92a6695b8
Zeo.Wang <zeowang131@outlook.com>
2023-04-15 09:43:05 -0700

    Added select photo functionality

--- frontend/VirtualCollection/src/pages/CollectionPage.tsx
+++ frontend/VirtualCollection/src/pages/CollectionPage.tsx
@@ -1,11 +1,32 @@
-import { Button, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
+import { Button, Image, Text, View } from 'react-native';
 import { AppStackProps } from './types';
 
 export default function CollectionPage({ navigation }: AppStackProps) {
+    const [selectedPicture, setSelectedPicture] = useState('');
+
+    const selectPicture = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedPicture(result.assets[0].uri);
+        }
+    };
+
     return (
         <View>
             <Text>Collection</Text>
             <Button title="Home" onPress={() => navigation.goBack()} />
+            <Button title="Select Picture" onPress={() => selectPicture()} />
+            {selectedPicture && (
+                <Image source={{ uri: selectedPicture }} style={{ width: 200, height: 200 }} />
+            )}
         </View>
     );
 }
