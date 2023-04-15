a7a25b4078e58c592dfe7cdce1dcf940c2181493
Zeo.Wang <zeowang131@outlook.com>
2023-04-15 10:16:40 -0700

    Added score field

--- frontend/VirtualCollection/src/pages/CollectionPage.tsx
+++ frontend/VirtualCollection/src/pages/CollectionPage.tsx
@@ -1,10 +1,12 @@
 import * as ImagePicker from 'expo-image-picker';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Image, Text, View } from 'react-native';
+import { Float } from 'react-native/Libraries/Types/CodegenTypes';
 import { AppStackProps } from './types';
 
 export default function CollectionPage({ navigation }: AppStackProps) {
     const [selectedPicture, setSelectedPicture] = useState('');
+    const [score, setScore] = useState<null | Float>(null);
 
     const selectPicture = async () => {
         const result = await ImagePicker.launchImageLibraryAsync({
@@ -17,13 +19,28 @@ export default function CollectionPage({ navigation }: AppStackProps) {
         if (!result.canceled) {
             setSelectedPicture(result.assets[0].uri);
         }
+        return result;
     };
+
+    const postPic = async (pic: ImagePicker.ImagePickerResult) => {
+        return pic ? 12.0 : 0.0;
+    };
+
+    useEffect(() => {
+        async function fetchScore() {
+            const pic = await selectPicture();
+            const score = await postPic(pic);
+            setScore(score);
+        }
+        fetchScore();
+    }, []);
 
     return (
         <View>
             <Text>Collection</Text>
             <Button title="Home" onPress={() => navigation.goBack()} />
             <Button title="Select Picture" onPress={() => selectPicture()} />
+            {score ? <p>The returned deata: {JSON.stringify(score)}</p> : <p>Loading...</p>}
             {selectedPicture && (
                 <Image source={{ uri: selectedPicture }} style={{ width: 200, height: 200 }} />
             )}
